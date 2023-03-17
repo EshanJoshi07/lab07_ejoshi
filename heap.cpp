b45d4b6b8474411574a28efe8e94c8e0b4b737fd
Eshan Joshi <eshanjoshi@cs.ucsb.edu>
2023-03-16 19:54:49 -0700

    working lab07

--- heap.cpp
+++ heap.cpp
@@ -1,33 +1,6 @@
 // heap.cpp
 // Diba Mirza
 
-// #include "heap.h"
-// #include <iostream>
-// using std::cout;
-
-// // Pushes a value into the heap, then ensures
-// // the heap is correctly arranged
-// void Heap::push(int value){
-
-// }
-
-// // Pops the minimum value off the heap
-// // (but does not return it), then ensures
-// // the heap is correctly arranged
-// void Heap::pop(){
-
-// }
-
-// // Returns the minimum element in the heap
-// int Heap::top(){
-//   return 0;
-// }
-
-// // Returns true if the heap is empty, false otherwise
-// bool Heap::empty(){
-//   return true;
-// }
-    
 #include "heap.h"
 #include <iostream>
 using std::cout;
@@ -35,21 +8,13 @@ using std::cout;
 // Pushes a value into the heap, then ensures
 // the heap is correctly arranged
 void Heap::push(int value){
-  // Add the new value to the end of the heap
   vdata.push_back(value);
-
-  // Get the index of the new value
-  int index = vdata.size() - 1;
-
-  // Fix the heap property by comparing the new value to its parent
-  while (index > 0 && vdata[(index - 1) / 2] > vdata[index]) {
-    // Swap the new value with its parent
-    int temp = vdata[(index - 1) / 2];
-    vdata[(index - 1) / 2] = vdata[index];
-    vdata[index] = temp;
-
-    // Update the index to the parent's index
-    index = (index - 1) / 2;
+  int i = vdata.size() - 1;
+  while (i > 0 && vdata[(i - 1) / 2] > vdata[i]) {
+    int temp = vdata[(i - 1) / 2];
+    vdata[(i - 1) / 2] = vdata[i];
+    vdata[i] = temp;
+    i = (i - 1) / 2;
   }
 }
 
@@ -60,29 +25,28 @@ void Heap::pop(){
   if (vdata.size() == 0) {
     return;
   }
-
-  // Replace the root with the last element in the heap
   vdata[0] = vdata.back();
   vdata.pop_back();
 
   // Fix the heap property by comparing the new root to its children
-  int index = 0;
-  while (index * 2 + 1 < vdata.size()) {
+  int i = 0;
+  while ((i * 2) + 1 < vdata.size()) {
     // Find the index of the smallest child
-    int min_child_index = index * 2 + 1;
-    if (index * 2 + 2 < vdata.size() && vdata[index * 2 + 2] < vdata[min_child_index]) {
-      min_child_index = index * 2 + 2;
+    int min_child_index = (i * 2) + 1;
+    if ((i * 2) + 2 < vdata.size() && vdata[(i * 2) + 2] < vdata[min_child_index]) {
+      min_child_index = (i * 2) + 2;
     }
 
     // If the parent is larger than the smallest child, swap them
-    if (vdata[index] > vdata[min_child_index]) {
-      int temp = vdata[index];
-      vdata[index] = vdata[min_child_index];
+    if (vdata[i] > vdata[min_child_index]) {
+      int temp = vdata[i];
+      vdata[i] = vdata[min_child_index];
       vdata[min_child_index] = temp;
 
       // Update the index to the smallest child's index
-      index = min_child_index;
-    } else {
+      i = min_child_index;
+    } 
+    else {
       break;
     }
   }
